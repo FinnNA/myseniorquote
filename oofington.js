ee36f9daa9a13bbaa7fad7d7c1c6d39470399b87
FinnNA <35965181+FinnNA@users.noreply.github.com>
2018-09-12 16:44:39 -0400

    oof

--- oofington.js
+++ oofington.js
@@ -3,8 +3,13 @@ var quotes=new Array()
 
 //change the quotes if desired. Add/ delete additional quotes as desired.
 
-quotes[0]='<i class="em em-ok_hand" style=" font-size:250px;"></i>'
+quotes[0]='<i class="em em-ok_hand" style=" font-size:200px;"></i>'
 quotes[1]='Whats the deal with airplane food.'
+quotes[2]='YA *voicecrack* YEET <i> -Marcus G. </i>'
+quotes[3]='Im a heavy breathy boy'
+quotes[4]='*whispery voice* "tricerathicc" <i> -Charles F.</i>'
+quotes[5]='Im a big girl now <i> -Charles F.</i>'
+
 
 
 var whichquote=Math.floor(Math.random()*(quotes.length))
